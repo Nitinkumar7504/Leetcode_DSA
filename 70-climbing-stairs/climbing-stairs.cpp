bfa77c5602503abb0469c09c1bb0cba6449903b8
Nitinkumar7504 <157092751+Nitinkumar7504@users.noreply.github.com>
2025-04-03 09:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (33.88%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+         if(n<3) return n;
+        vector<int> v(n,0);
+        v[0]=1;
+        v[1]=2;
+        for(int i=2;i<n;i++) v[i] = v[i-1]+v[i-2];
+        return v[n-1];
+    }
+};
